9a4b0f87e5a1a8824ccd222a6043b1cb96369156
GuilhermeSChorr <guilherme.schorr@universo.univates.br>
2025-09-10 21:53:44 -0300

    add

--- atividade_dupla/app/index.js
+++ atividade_dupla/app/index.js
@@ -4,7 +4,12 @@ import { Link } from "expo-router";
 export default function Index() {
     return (
         <View>
-            
+             <Link href="/guilherme" asChild>
+                <Button title="Clique para ver o app do Guilherme"  />  
+             </Link>
+             <Link href="/fernando" asChild>
+                <Button title="Clique para ver o app do Fernando"  />  
+             </Link>
         </View>
     )
 }
