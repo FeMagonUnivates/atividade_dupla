214ef3f81082a00eb449b0cba6145143aa9344fc
FERNANDO MAGON WEIZENMANN <fernando.weizenmann@acad.univates>
2025-09-10 21:18:19 -0300

    início

---
+++ atividade_dupla/app/index.js
@@ -0,0 +1,10 @@
+import { Button, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+    return (
+        <View>
+            
+        </View>
+    )
+}
